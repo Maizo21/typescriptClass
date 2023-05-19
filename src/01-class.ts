ab49518e420d479bcb55a619e8628e160d9b72b2
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2023-05-19 17:07:52 -0400

    primer archivo, clases

---
+++ src/01-class.ts
@@ -0,0 +1,13 @@
+class MyDate {
+  day: number;
+  month: number;
+  year: number;
+
+  constructor(day: number, month: number, year: number) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+}
+
+const myDate = new MyDate(1, 1, 2023);
